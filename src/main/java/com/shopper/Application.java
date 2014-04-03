8160bd0ab27dfce065c6e22807d1f46a3ac786e0
Brian Susko <brian.susko@gmail.com>
2014-04-03 15:54:00 -0400

    updated comment

--- src/main/java/com/shopper/Application.java
+++ src/main/java/com/shopper/Application.java
@@ -3,7 +3,7 @@ package com.shopper;
 public class Application {
 
 	public static void main(String[] args){
-		System.out.println("Testing git integration");
+		System.out.println("Testing git integration with update");
 	}
 	
 }
